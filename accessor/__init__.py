eb75ab931124cc4a892f550321ebc5145e14ceb1
Jakub Jagielka <jjagielka@gmail.com>
2025-09-07 16:03:56 +0200

    clean up

--- accessor/__init__.py
+++ accessor/__init__.py
@@ -8,11 +8,15 @@ from typing import Any, Callable, Dict, List, Optional, Union
 
 import operator
 
-ops = ['__lt__', '__le__', '__eq__', '__ne__', '__ge__', '__gt__', '__not__',
-       '__abs__', '__add__', '__and__', '__floordiv__', '__index__', '__inv__',
-       '__invert__', '__lshift__', '__mod__', '__mul__', '__matmul__',
-       '__neg__', '__or__', '__pos__', '__pow__', '__rshift__', '__sub__',
-       '__truediv__', '__xor__', '__concat__', '__contains__']
+ops = [
+    "__abs__", "__add__", "__all__", "__and__", "__concat__", "__contains__", "__delitem__",
+    "__eq__", "__floordiv__", "__ge__", "__gt__", "__iadd__", "__iand__", "__iconcat__",
+    "__ifloordiv__", "__ilshift__", "__imatmul__", "__imod__", "__imul__", "__index__",
+    "__inv__", "__invert__", "__ior__", "__ipow__", "__irshift__", "__isub__", "__itruediv__",
+    "__ixor__", "__le__", "__lshift__", "__lt__", "__matmul__", "__mod__", "__mul__","__ne__",
+    "__neg__", "__not__", "__or__", "__pos__", "__pow__", "__rshift__", "__sub__", "__truediv__",
+    "__xor__",
+]  # fmt: skip
 
 
 class Meta(type):
@@ -25,6 +29,7 @@ class Meta(type):
             @wraps(func)
             def inner(self, *args, **kwargs):
                 return lambda x: func(self(x), *args, **kwargs)
+
             return inner
 
         for name in ops:
@@ -33,7 +38,7 @@ class Meta(type):
 
 def _str_slice(s):
     t = (s.start, s.stop, s.step) if s.step else (s.start, s.stop)
-    return ':'.join(['' if x is None else str(x) for x in t])
+    return ":".join(["" if x is None else str(x) for x in t])
 
 
 class Accessor(metaclass=Meta):
@@ -43,9 +48,9 @@ class Accessor(metaclass=Meta):
     After f = Accessor.foo.bar, the call f(r) returns r["foo"]["bar"].
     """
 
-    __slots__ = ('_accessor', '_name', '_path')
+    __slots__ = ("_accessor", "_name", "_path")
 
-    def __init__(self, accessor: Callable = None, name: str="", path: str="") -> None:
+    def __init__(self, accessor: Callable = None, name: str = "", path: str = "") -> None:
         self._accessor = accessor
         self._name = name
         self._path = path
@@ -53,12 +58,12 @@ class Accessor(metaclass=Meta):
     def __call__(self, x: Any) -> Any:
         return x if self._accessor is None else self._accessor(x)
 
-    def __getattr__(self, name: str) -> 'Accessor':
+    def __getattr__(self, name: str) -> "Accessor":
         return self.__getitem__(name)
 
-    def __getitem__(self, name: Union[str, int, slice]) -> 'Accessor':
+    def __getitem__(self, name: Union[str, int, slice]) -> "Accessor":
         def accessor(x: Any) -> Any:
-            value = x if self._accessor is None else self._accessor(x) # self(x)
+            value = x if self._accessor is None else self._accessor(x)  # self(x)
             try:
                 if not isinstance(name, (int, slice)) and isinstance(value, list):
                     return [v[name] for v in value]
@@ -74,7 +79,7 @@ class Accessor(metaclass=Meta):
         else:
             _name = str(name)
             _path = f"{self._path}.{_name}" if self._path else _name
-            
+
         return self.__class__(accessor, _name, _path)
 
     def __repr__(self) -> str:
@@ -84,81 +89,30 @@ class Accessor(metaclass=Meta):
 accessor = Accessor()
 
 
-def values(*getters: 'Accessor') -> Callable[[Any], tuple]:
+def values(*getters: "Accessor") -> Callable[[Any], tuple]:
     """Return func extracting values of multiple getters as tuple."""
     return lambda x: tuple(n(x) for n in getters)
 
 
-def keys(*getters: 'Accessor') -> Callable[[Any], tuple]:
+def keys(*getters: "Accessor") -> Callable[[Any], tuple]:
     """Return paths."""
     return lambda x: tuple(n._path for n in getters)
 
 
-def items(*getters: 'Accessor', prefix: str = '') -> Callable[[Any], Dict[str, Any]]:
+def items(*getters: "Accessor", prefix: str = "") -> Callable[[Any], Dict[str, Any]]:
     """Return func extracting names and values of multiple getters as tuple."""
     if prefix:
         return lambda x: {prefix + n._name: n(x) for n in getters}
     return lambda x: {n._name: n(x) for n in getters}
 
 
-def select(*getters: 'Accessor', **name_getters: 'Accessor') -> Callable[[Any], Dict[str, Any]]:
+def select(*getters: "Accessor", **name_getters: "Accessor") -> Callable[[Any], Dict[str, Any]]:
     """Return func extracting values of multiple getters as dict.
 
     getters: list of getters for dict entries with _name key
     name_getters: list of getters with names
     """
-    return lambda x: dict(
-        {n._name: n(x) for n in getters},
-        **{k: f(x) for k, f in name_getters.items()})
-
-
-def flatten(list_getter: 'Accessor', *getters: 'Accessor', **kwargs: Any) -> Callable[[Any], List[Dict[str, Any]]]:
-    """Flatten nested data structures with optional field mapping.
-    
-    Parameters
-    ----------
-    list_getter : Accessor
-        Accessor that returns a list to flatten
-    *getters : Accessor
-        Additional accessors to include in flattened result
-    **kwargs : dict
-        Additional fields to include, where keys are field names and values are accessors
-        Special keyword 'suffix' can be used to add suffix to list_getter field names
-        
-    Returns
-    -------
-    function
-        A function that takes data and returns flattened list of dictionaries
-    """
-    suffix = kwargs.pop('suffix', '')
-    
-    def flattener(data: Any) -> List[Dict[str, Any]]:
-        result = []
-        items = list_getter(data) or []
-        
-        for item in items:
-            flat_item = {}
-            
-            # Add fields from getters
-            for getter in getters:
-                if hasattr(getter, '_name'):
-                    flat_item[getter._name] = getter(data)
-            
-            # Add fields from kwargs (excluding suffix)
-            for key, getter in kwargs.items():
-                if hasattr(getter, '_name'):
-                    flat_item[key] = getter(data)
-            
-            # Add fields from the list item with optional suffix
-            if isinstance(item, dict):
-                for key, value in item.items():
-                    flat_item[key + suffix] = value
-            
-            result.append(flat_item)
-        
-        return result
-    
-    return flattener
+    return lambda x: dict({n._name: n(x) for n in getters}, **{k: f(x) for k, f in name_getters.items()})
 
 
 def normalize(data, c_getter, select, c_select):
@@ -174,4 +128,3 @@ def normalize(data, c_getter, select, c_select):
     """
     for x in [data] if isinstance(data, dict) else data:
         yield from (dict(select(x), **c_select(c)) for c in c_getter(x) or [{}])
-
--- tests/test.py
+++ tests/test.py
@@ -2,31 +2,30 @@
 
 import unittest
 
-from accessor import accessor as _, flatten
+from accessor import accessor as _, normalize
 
 
 tester = [
     {
-        'a': 1,
-        'b': {'c': 5, 'cc': {'d': 6, 'e': 7}},
-        'f': [
-            {'g': 11, 'h': 12},
-            {'g': 13, 'h': 14},
-        ]
+        "a": 1,
+        "b": {"c": 5, "cc": {"d": 6, "e": 7}},
+        "f": [
+            {"g": 11, "h": 12},
+            {"g": 13, "h": 14},
+        ],
     },
     {
-        'a': 3,
-        'b': {'c': 8, 'cc': {'d': 9, 'e': 10}},
-        'f': [
-            {'g': 21, 'h': 22},
-            {'g': 23, 'h': 24},
-        ]
-    }
+        "a": 3,
+        "b": {"c": 8, "cc": {"d": 9, "e": 10}},
+        "f": [
+            {"g": 21, "h": 22},
+            {"g": 23, "h": 24},
+        ],
+    },
 ]
 
 
 class TestGetter(unittest.TestCase):
-
     def test_identity(self):
         self.assertEqual(_(tester[0]), tester[0])
         self.assertEqual(list(map(_, tester)), tester)
@@ -46,16 +45,8 @@ class TestGetter(unittest.TestCase):
     def test_filter_level(self):
         self.assertEqual(list(filter(_.b.cc.d > 7, tester)), tester[1:])
 
-    def test_flatten(self):
-        result = [
-            [{'a': 1, 'c': 5, 'g_c': 11, 'h_c': 12},
-             {'a': 1, 'c': 5, 'g_c': 13, 'h_c': 14}],
-            [{'a': 3, 'c': 8, 'g_c': 21, 'h_c': 22},
-             {'a': 3, 'c': 8, 'g_c': 23, 'h_c': 24}]]
-        self.assertEqual(list(map(flatten(_.f[:], _.a, c=_.b.c, suffix='_c'), tester)), result)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
 
 
